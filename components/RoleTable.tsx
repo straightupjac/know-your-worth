784b5567414a18ec7dc48743982b4d594f888de2
straightupjac <jdc.jaclyn@gmail.com>
2022-07-10 17:30:58 -0700

    paginate

--- components/RoleTable.tsx
+++ components/RoleTable.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/jsx-key */
 import * as React from "react";
 import {
   Table,
@@ -8,13 +7,17 @@ import {
   Th,
   Td,
   chakra,
-  Box
+  Box,
+  Button,
+  Input,
+  Select,
+  HStack
 } from "@chakra-ui/react";
 import {
   TriangleDownIcon,
   TriangleUpIcon
 } from "@chakra-ui/icons";
-import { useTable, useSortBy, Column, Row } from "react-table";
+import { useTable, useSortBy, Column, usePagination } from "react-table";
 import { CompData, CompHeaders } from "@utils/roleTypes";
 import useSWR from 'swr';
 
@@ -25,23 +28,42 @@ export type DataTableProps<Data extends object> = {
 
 function RoleTable<Data extends object>({
   data,
-  columns
+  columns,
 }: DataTableProps<Data>) {
   const {
     getTableProps,
     getTableBodyProps,
     headerGroups,
-    rows,
-    prepareRow
-  } = useTable({ columns, data }, useSortBy);
+    prepareRow,
+    pageOptions,
+    page,
+    state: { pageIndex, pageSize },
+    gotoPage,
+    previousPage,
+    pageCount,
+    nextPage,
+    setPageSize,
+    canPreviousPage,
+    canNextPage,
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: { pageIndex: 0 }, // Pass our hoisted table state
+    },
+    useSortBy,
+    usePagination,
+  )
 
   return (
     <Box p={4} width='100%'>
       <Table {...getTableProps()} variant='simple'>
         <Thead>
           {headerGroups.map((headerGroup) => (
+            // eslint-disable-next-line react/jsx-key
             <Tr {...headerGroup.getHeaderGroupProps()}>
               {headerGroup.headers.map((column) => (
+                // eslint-disable-next-line react/jsx-key
                 <Th
                   {...column.getHeaderProps(
                     column.getSortByToggleProps()
@@ -63,23 +85,80 @@ function RoleTable<Data extends object>({
           ))}
         </Thead>
         <Tbody {...getTableBodyProps()}>
-          {rows.map((row) => {
-            prepareRow(row);
+          {page.map((row, i) => {
+            prepareRow(row)
             return (
+              // eslint-disable-next-line react/jsx-key
               <Tr {...row.getRowProps()}>
                 {row.cells.map((cell) => (
-                  <Td
-                    {...cell.getCellProps()}
-                  >
-                    {cell.render("Cell")}
+                  // eslint-disable-next-line react/jsx-key
+                  <Td {...cell.getCellProps()}>
+                    {cell.render('Cell')}
                   </Td>
                 ))}
               </Tr>
-            );
+            )
           })}
+          <Tr>
+            {!data ? (
+              // Use our custom loading state to show a loading indicator
+              <Td colSpan={10000}>Loading...</Td>
+            ) : (
+              <Td colSpan={10000}>
+                Showing {page.length} of ~ {pageCount * pageSize}{' '}results
+              </Td>
+            )}
+          </Tr>
         </Tbody>
       </Table>
-    </Box>
+      <HStack paddingTop={2}>
+        <Button size="xs" variant='ghost' onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </Button>{' '}
+        <Button size="xs" variant='ghost' onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {'<'}
+        </Button>{' '}
+        <Button size="xs" variant='ghost' onClick={() => nextPage()} disabled={!canNextPage}>
+          {'>'}
+        </Button>{' '}
+        <Button size="xs" variant='ghost' onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </Button>{' '}
+        <span>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{' '}
+        </span>
+        <span>
+          | Go to page:{' '}
+          <Input
+            type="number"
+            size="xs"
+            defaultValue={pageIndex + 1}
+            onChange={e => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0
+              gotoPage(page)
+            }}
+            style={{ width: '100px' }}
+          />
+        </span>{' '}
+        <Select
+          value={pageSize}
+          maxWidth='300px'
+          onChange={e => {
+            setPageSize(Number(e.target.value))
+          }}
+          size="xs"
+        >
+          {[10, 20, 30, 40, 50].map(pageSize => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </Select>
+      </HStack>
+    </Box >
   );
 }
 
@@ -120,23 +199,23 @@ export const RoleOverview = () => {
   const columns: Column<CompHeaders>[] = [
     {
       Header: "Job Title",
-      accessor: "jobTitle.value",
+      accessor: "jobTitle",
     },
     {
-      Header: "Company type",
-      accessor: "companyType.value",
+      Header: "Company Type",
+      accessor: "companyType",
     },
     {
       Header: "Company Stage",
-      accessor: "companyStage.value"
+      accessor: "companyStage"
     },
     {
       Header: "Employment Type",
-      accessor: "employmentType.value"
+      accessor: "employmentType"
     },
     {
       Header: "Annual Base",
-      accessor: "annualBase.value"
+      accessor: "annualBase"
     },
   ];
 
