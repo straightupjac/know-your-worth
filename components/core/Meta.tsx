29a610970d4ec17478de193e12f9ee173299ec03
straightupjac <jdc.jaclyn@gmail.com>
2022-09-08 15:20:46 -0700

    fix meta

--- components/core/Meta.tsx
+++ components/core/Meta.tsx
@@ -3,6 +3,7 @@ import Head from "next/head"
 export const Meta = () => {
   return (
     <Head>
+      <title>Know Your Worth in Web3</title>
       <meta name="title" content="Know Your Worth in Web3" />
       <meta name="description" content="Have you ever received a job offer and struggled to figure out if it was actually one worth taking? If so, you're not alone." />
       <link rel="icon" href="/favicon.ico" />
